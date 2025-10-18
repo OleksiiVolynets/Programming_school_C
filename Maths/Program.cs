4c9cfea27726def23473ab9ad802fb1f4ce65e32
OleksiiVolynets <pereguznya@gmail.com>
2025-10-18 19:17:02 +0200

    create a new folder

---
+++ Maths/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Project
+{
+    
+    class Program
+    {
+        static void Main()
+        {
+            //Математичні дії
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            double result = a + b;
+            Console.WriteLine("The result is " + result);
+        }
+    }
+
+}
