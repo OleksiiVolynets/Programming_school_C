82fc1db213752dcd9c45fa3af65930e6e744900c
OleksiiVolynets <pereguznya@gmail.com>
2025-10-18 17:17:04 +0200

    create file

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Project
+{
+    
+    class Program
+    {
+        static void Main()
+        {
+            //Comment fejijeijewaijeijifwj
+            Console.Write("Hello\n");
+            Console.Write("111\n");
+            Console.Write("!!! ");
+        }
+    }
+
+}
