d5edf8c8c062e752edd3a9feeca7dac67666c2d5
OleksiiVolynets <pereguznya@gmail.com>
2025-10-19 09:10:14 +0200

    create if-else project

---
+++ if-else/Program.cs
@@ -0,0 +1,37 @@
+﻿﻿using System;
+using System.Globalization;
+namespace Project
+{
+    
+    class Program
+    {
+        static void Main()
+        {
+            //Умовні коеструкції
+            // Console.WriteLine("How old are you:");
+            // double a = Convert.ToDouble(Console.ReadLine());
+            // bool drink = true;
+            // // &&-and ||-or !-not 
+            // if (a < 18 && drink)
+            // {
+
+            //     Console.WriteLine("Go away, drink some milk");
+
+            //     Console.WriteLine("You are teenager");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("You are adult");
+            // }
+
+            Console.WriteLine("What's your name:");
+            string role = Console.ReadLine();
+
+            if (role == "Admin")
+            {
+                Console.WriteLine("Username:");
+            }
+        }
+    }
+
+}
