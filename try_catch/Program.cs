b689785134cb66037bf423c45ceaa424bc2f24f4
OleksiiVolynets <pereguznya@gmail.com>
2025-11-10 11:04:34 +0100

    create try_catch project with try catch example

---
+++ try_catch/Program.cs
@@ -0,0 +1,47 @@
+﻿﻿using System;
+
+namespace Project
+{
+    
+    class Program
+    {
+        static void Main()
+        {
+            //Try catch
+            // try
+            // {
+            //     int num = Convert.ToInt32(Console.ReadLine());
+            //     Console.WriteLine(num);
+            // }
+            // catch(FormatException)
+            // {
+            //     Console.WriteLine("You entered not a number");
+            // }
+            bool IsRunn = true;
+            while (IsRunn)
+            {
+                try
+                {
+                    Console.WriteLine("Enter a number:");
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    float rezult = 100 / num;
+                    Console.WriteLine(rezult);
+                    IsRunn = false;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("You entered not a number");
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("You can't divide by zero");
+                }
+                finally
+                {
+                    Console.WriteLine("End of iteration");
+                }
+            }    
+        }
+    }
+
+}
